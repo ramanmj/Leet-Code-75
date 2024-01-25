42caf150d5893e6a5f47e41d5f26a062b441106a
ramanmj <ramanmaharjan97@gmail.com>
2024-01-25 21:49:54 +0545

    init Merge String alternately

---
+++ leet75/Array_Strings/KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet75.Array_Strings
+{
+    internal class KidsWithTheGreatestNumberOfCandies
+    {
+    }
+}
---
+++ leet75/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using leet75.Array_Strings;
+using System.ComponentModel.Design;
+
+//for question 1/75 Merge Alternately 
+
+MergeStringAlternately sol1 = new MergeStringAlternately();
+var mergeALternately = sol1.MergeAlternately("aaa", "sdf");
+Console.WriteLine("Solution for Qno 1 :"+mergeALternately);
+
+//For question 2/75 
+
+//For question 3/75 
+
+//For question 4/75 
+
+//For question 5/75 
+
+//For question 6/75 
+
+//For question 7/75 
+
+//For question 8/75 
+
+//For question 9/75 
